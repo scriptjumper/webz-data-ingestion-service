2453825af6da6131becd32277e0309efd10be2e1
Shaeen Singh <shaeendev@gmail.com>
2025-07-13 15:37:24 +0200

    feat(webz): add WebzService with pagination, transformation and query builder

---
+++ src/services/WebzQueryBuilder.ts
@@ -0,0 +1,34 @@
+export class WebzQueryBuilder {
+    private baseUrl: string;
+    private token: string;
+    private query: string = '';
+    private language?: string;
+
+    constructor(token: string, baseUrl: string) {
+        this.token = token;
+        this.baseUrl = baseUrl;
+    }
+
+    setQuery(query: string): this {
+        this.query = query;
+        return this;
+    }
+
+    setLanguage(language: string): this {
+        this.language = language;
+        return this;
+    }
+
+    buildUrl(): string {
+        const params = new URLSearchParams({
+            token: this.token,
+            q: this.query,
+        });
+
+        if (this.language) {
+            params.append('language', this.language);
+        }
+
+        return `${this.baseUrl}?${params.toString()}`;
+    }
+}
