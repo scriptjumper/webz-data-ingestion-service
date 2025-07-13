cee9f20b6fff5d3462e7c910d858233941464c16
Shaeen Singh <shaeendev@gmail.com>
2025-07-13 15:11:56 +0200

    feat(db): configure PostgreSQL connection using pg and environment variables

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log('Webz Data Ingestion Service started');
+import pool from './config/database';
+
+async function testConnection() {
+  const res = await pool.query('SELECT NOW()');
+  console.log('DB Time:', res.rows[0]);
+  process.exit();
+}
+
+testConnection();
